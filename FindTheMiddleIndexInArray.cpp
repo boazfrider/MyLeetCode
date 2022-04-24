c174db0c82eba1c9878b8ff3ba31ca58f571f645
boazfrider <boazfrider@gmail.com>
2022-04-24 10:46:52 +0300

    q2

---
+++ FindTheMiddleIndexInArray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int n=nums.size();
+        int total=0;
+        
+        for(int num : nums)
+            total+=num;
+        int sum=0;
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(sum==total-sum-nums[i])
+                return i ;
+            sum+=nums[i];
+        }
+        return -1;
+    }
+};
