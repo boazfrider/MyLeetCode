43093996ee807c6d4db749510387f94dc01f100c
boazfrider <boazfrider@gmail.com>
2022-04-24 20:42:12 +0300

    20. Valid Parentheses

---
+++ ValidParentheses.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> st;
+        
+        for(int i = 0 ; i<s.length() ; i++)
+        {
+            if(s[i]=='(' ||s[i]=='{' ||s[i]=='[' )
+                st.push(s[i]);
+            if(s[i]==')'){
+                 if(st.empty())
+                     return false;
+                if(st.top()!='(')
+                    return false;
+                else
+                    st.pop();
+            }
+             if(s[i]=='}'){
+                 if(st.empty())
+                     return false;
+                if(st.top()!='{')
+                    return false;
+                else
+                    st.pop();
+            }
+            
+             if(s[i]==']'){
+                 if(st.empty())
+                     return false;
+                if(st.top()!='[')
+                    return false;
+                else
+                    st.pop();
+            }
+        }
+        if(st.empty()==false)
+            return false;
+        return true;
+    }
+};
