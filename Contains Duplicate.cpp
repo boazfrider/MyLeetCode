a213b5a32321c3cdfb4eb16ca58ac85410bd4083
boazfrider <boazfrider@gmail.com>
2022-04-25 21:26:58 +0300

    217. Contains Duplicate

---
+++ Contains Duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> counter;
+        
+        for(int i = 0 ; i < nums.size() ; i ++)
+        {
+            counter[nums[i]]++;
+            if(counter[nums[i]]>1)
+                return true;
+        }
+        return false;
+        
+    }
+};
