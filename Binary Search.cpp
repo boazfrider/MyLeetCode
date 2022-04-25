887cdd7c1fcc5ba7aa5f5f44fcb716123fdaea21
boazfrider <boazfrider@gmail.com>
2022-04-25 21:37:00 +0300

    704. Binary Search

---
+++ Binary Search.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start=0;
+        int end=nums.size()-1;
+        int middle;
+        while(start<=end)
+        {
+            middle=(start+end)/2;
+            if(nums.at(middle)==target)
+                return middle;
+            if(nums.at(middle)<target)
+                start=middle+1;
+            else
+                end=middle-1;
+        }
+        return -1;
+        
+    }
+};
