6ce1e98193fdfb462fe305cdd1e85eb1b9ae4028
boazfrider <boazfrider@gmail.com>
2022-04-25 21:20:36 +0300

    2011. Final Value of Variable After Performing Operations

---
+++ FinalValueofVariableAfterPerformingOperations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x=0;
+        
+        for(string str : operations)
+        {
+            if(str=="++X" || str=="X++")
+                x++;
+            else
+                x--;
+        }
+        return x;
+    }
+};
